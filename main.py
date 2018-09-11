67cc3e5b401b6f6accf04517313cc3b67759b5ea
Hansen Johnson <hansenjohnson@Hansens-MacBook-Pro.local>
2018-09-11 16:03:05 -0300

    check this out

---
+++ main.py
@@ -0,0 +1,8 @@
+name = input('What is your name?')
+print('Your name is ' + name)
+
+age = int(input('What is your age?'))
+year = str((2018 - age)+100)
+
+
+print('Hi' + name + ', you will be 100 in the year' + year)
